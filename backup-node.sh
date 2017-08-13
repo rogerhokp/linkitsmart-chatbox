fb34116ff3945100f79f694a42345b8ae9f8d21c
RogerHo <rogerhokp@gmail.com>
2017-08-13 17:33:46 +0800

    update gitIngore add nodejs backup script

---
+++ backup-node.sh
@@ -0,0 +1,7 @@
+mkdir -p /Media/USB-A1/node6/usr/lib/node/
+cp -r /usr/lib/node/* /Media/USB-A1/node6/usr/lib/node
+
+mkdir -p /Media/USB-A1/node6/usr/lib/node_modules/
+cp -r /usr/lib/node_modules/* /Media/USB-A1/node6/usr/lib/node_modules
+
+cp /usr/bin/node /Media/USB-A1/node6/
